a784f20e138a5c84e4562e5688787d358ace016b
anewlifebji-sketch <anewlifebji@gmail.com>
2026-08-30 18:59:16 +0330

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.storepricelist.app',
-  appName: 'Store Price List',
-  webDir: 'dist'
-};
-
-export default config;
